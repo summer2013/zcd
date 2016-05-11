22b053a97fcb4ef99da0cca6d24394c27f160996
summer <shiny_mi@163.com>
2016-05-11 13:57:41 +0800

    add ui-router

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var app = express();
+var indexApp = angular.module('indexApp', ["ui.router"]);
+indexApp.config(function($stateProvider, $urlRouterProvider){
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.use(express.static('./'));
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  // For any unmatched url, send to /route1
+  $urlRouterProvider.otherwise("/home");
+  $stateProvider
+  .state('home', {
+    url: "/home",
+    templateUrl: "home.html",
+    controller: function($scope) {
+      $scope.showSignModal = false;
+      $scope.openSignModal = function() {
+        $scope.showSignModal = true;
+      }
+      $scope.close = function() {
+        $scope.showSignModal = false;
+      }
+      $scope.weekdays = ['日', '一', '二', '三', '四', '五', '六'];
+      $scope.days = [[26, 27, 28, 29, 30, 31, 1],
+        [2, 3, 4, 5, 6, 7, 8],
+        [9, 10, 11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20, 21, 22],
+        [23, 24, 25, 26, 27, 28, 29],
+        [30, 1, 2, 3, 4, 5, 6]];
+    }
+  })
+})
