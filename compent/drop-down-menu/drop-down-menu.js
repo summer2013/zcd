e35588005c1a8888ff8d853675bda3c9f23e8e72
summer <shiny_mi@163.com>
2016-05-13 15:10:31 +0800

    finish dropDownMenu

--- compent/drop-down-menu/drop-down-menu.js
+++ compent/drop-down-menu/drop-down-menu.js
@@ -9,11 +9,41 @@ dropDownMenu.controller('DropDownMenuController', ['$scope', function($scope) {
       {'name': '滴滴出行'},
       {'name': '测试专用'}]},
     {'name': '淘宝优惠', 'children': [{'name': '全部'}]}];
-  $scope.subMenus2 =  ['兑换次数从高到低', '会员积分总动员从低到高', '会员积分总动员从高到低'];
+  $scope.subMenus2 =  [{'name':'兑换次数从高到低'},
+    {'name':'会员积分总动员从低到高'},
+    {'name':'会员积分总动员从高到低'}];
 
   $scope.showList = false;
-  $scope.switchMenu = function() {
+  $scope.headerActive = false;
+  $scope.switchList = function() {
     $scope.showList = !$scope.showList;
+    $scope.headerActive = !$scope.headerActive;
+    $scope.showExchangeList = false;
+    $scope.exchangeActive = false;
+    $scope.mineExchangeActive = false;
   }
 
-}]);
+  $scope.showExchangeList = false;
+  $scope.exchangeActive = false;
+  $scope.switchExchangeList = function() {
+    $scope.showExchangeList = !$scope.showExchangeList;
+    $scope.exchangeActive = !$scope.exchangeActive;
+    $scope.showList = false;
+    $scope.headerActive = false;
+    $scope.mineExchangeActive = false;
+  }
+
+  $scope.mineExchangeActive = false;
+  $scope.switchMineExchange = function() {
+    $scope.mineExchangeActive = !$scope.mineExchangeActive;
+    $scope.showList = false;
+    $scope.headerActive = false;
+    $scope.showExchangeList = false;
+    $scope.exchangeActive = false;
+  }
+}])
+.directive('dropDownMenu', function(){
+  return {
+    templateUrl: 'drop-down-menu.html'
+  }
+});
